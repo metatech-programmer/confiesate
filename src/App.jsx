4083e7bf270c13d9aadf94877307ab0192c26235
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2025-03-30 21:38:50 -0500

    refactor App component to include Menu, BtnTop, Footer, and Masonry with dynamic items

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import BtnTop from "./components/BtnTop";
+import Footer from "./components/Footer";
 import LandingTitle from "./components/LandingTitle";
+import Masonry from "./components/Masonry";
+import Menu from "./components/Menu";
 
 function App() {
+  
+  const items = Array.from({ length: 50 }, (_, i) => ({
+    id: i + 1,
+    alt: `Un chico de 18 años con una cerveza en la mano ${i + 1}`,
+  }));
+
   return (
-    <div className="w-full bg-stone-100">
+    <div className="w-full bg-gradient-to-b from-transparent from-70%  to-app-bluePurple/50 pb-24 relative">
       <LandingTitle />
+      <Menu />
+      <Masonry items={items} />
+      <BtnTop />
+      <Footer/>
     </div>
   );
 }
