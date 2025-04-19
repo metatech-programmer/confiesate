42b97bd8ffa922d83b9c2cdf58e52da4870912d4
Santiago Aguilar <97856988+metatech-programmer@users.noreply.github.com>
2025-04-18 21:15:53 -0500

    update files

--- app/src/types/Item.ts
+++ app/src/types/Item.ts
@@ -13,19 +13,3 @@ export interface Item {
   reports?: number;
 }
 
-export const transformToItem = (publication: any): Item => {
-  return {
-    id: publication.id,
-    author: publication.user?.name || 'Anónimo',
-    title: publication.title || '',
-    description: publication.content, // El contenido desencriptado
-    imageUrl: publication.image_url || '',
-    likes: publication.Like?.length || 0,
-    createdAt: publication.created_at.toISOString(),
-    comments: publication.Comment?.map((comment: any) => ({
-      author: comment.user?.name || 'Anónimo',
-      text: comment.comment_content // El contenido desencriptado
-    })) || [],
-    reports: publication.reports?.length || 0
-  };
-};
